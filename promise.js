7308b08e6e475ea604dccdd65bd16c0036f0dab1
Gilang Windu Asmara <gilangwinduasmara2@gmail.com>
2022-09-18 15:28:52 +0700

    feat: calculate the number of movies based on emotions

--- promise.js
+++ promise.js
@@ -1,7 +1,17 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emotion) => {
+  const theaterIXX = await promiseTheaterIXX();
+  const theaterVGC = await promiseTheaterVGC();
+  const emotions = [...theaterIXX, ...theaterVGC].reduce((acc, curr) => {
+    if (curr.hasil === emotion) {
+      acc += 1;
+    }
+    return acc;
+  }, 0);
+  return emotions;
+};
 
 module.exports = {
   promiseOutput,
